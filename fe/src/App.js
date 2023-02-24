19aa46ef6913364a44fccd642ba613dbdf2fb3fc
Peacefire1 <ganzorigenkhtsogt@gmail.com>
2023-02-24 09:55:54 +0800

    Admin-Panel start

---
+++ fe/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Space } from "antd";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import SideMenu from "./components/SideMenu/SideMenu";
+import PageContent from "./components/PageContent/PageContent";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Space>
+        <SideMenu></SideMenu>
+        <PageContent></PageContent>
+      </Space>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ fe/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import { Image } from "antd";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="AppHeader">
+      <Image
+        width={40}
+        src="https://cdn-icons-png.flaticon.com/512/1819/1819863.png"
+      ></Image>
+    </div>
+  );
+};
+
+export default Header;
